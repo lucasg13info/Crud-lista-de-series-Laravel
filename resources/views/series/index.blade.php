4c85aaf877b2a880163767c76a9898b821171a78
Lucas Estefano <lucccasestefano_@hotmail.com>
2021-10-15 13:53:00 -0300

    Tem certeza que deseja apagar?

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -16,7 +16,8 @@ Séries
         <ul class="list-group">
             @foreach ($series as $serie)
             <li class="list-group-item">{{ $serie -> nome }} 
-                <form method="post" action="/series/remover/{{ $serie->id }}">
+                <form method="post" action="/series/remover/{{ $serie->id }}"
+                    onsubmit="return confirm('Tem certeza que deseja apagar?')">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger">Excluir</button>
